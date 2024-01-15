14461862241fae75f9690b0e221d2e053055e033
Devadharshini <deva@machdatum.com>
2024-01-15 21:08:56 +0530

    menu add on closed bug resolved

--- src/pages/holidaycheck.tsx
+++ src/pages/holidaycheck.tsx
@@ -1,9 +1,29 @@
 import { useEffect, useState } from "react";
 import { DateTime } from "luxon";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const useHolidayCheck = () => {
   const [currentTime, setCurrentTime] = useState(DateTime.local());
   const [holiday, setHoliday] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get("https://backend-rung.onrender.com/shop_time_list/")
+      .then((response) => {
+        // console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Error in fetching data");
+      });
+  };
 
   useEffect(() => {
     const fetchHolidayData = async () => {
@@ -33,9 +53,18 @@ const useHolidayCheck = () => {
   const isClosed = () => {
     // Format current date and time to match the holiday data format
     const currentDateTimeFormatted = currentTime.toISO();
+    const currentDate = currentTime.toJSDate();
+    const currentShopTimings = data[0];
 
-    const isWithinSpecifiedHours =
-      currentTime.hour < 17 || currentTime.hour >= 21;
+    const startTime = DateTime.fromISO(
+      currentShopTimings?.shop_opening_time,
+    ).toJSDate();
+    const endTime = DateTime.fromISO(
+      currentShopTimings?.shop_closing_time,
+    ).toJSDate();
+
+    const isNoService =
+      currentDate < startTime || currentDate >= endTime;
 
     // console.log("isWithinSpecifiedHours:", isWithinSpecifiedHours);
 
@@ -56,7 +85,7 @@ const useHolidayCheck = () => {
 
     // console.log("holidayCheck:", holidayCheck);
 
-    return isWithinSpecifiedHours || holidayCheck;
+    return isNoService || holidayCheck;
   };
 
   const getHolidayNoteForCurrentTime = () => {
